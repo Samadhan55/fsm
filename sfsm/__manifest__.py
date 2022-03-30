b8122f758164bc0f10ba957d7012636268999c9d
Samadhan55 <Samadhan55@github.com>
2022-03-30 19:30:51 +0545

    First test. adding notes and changing name

--- sfsm/__manifest__.py
+++ sfsm/__manifest__.py
@@ -1,26 +1,29 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "smarten/sfsm",
+    'name': "FS-ISP",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        1.Create necessary projects and stages (data) for managing different type of tasks.
+        3.Adding fields(in res.partner, project.task) and models()
+        3.flow automation and customization for each type of project.""",
 
     'description': """
-        Long description of module's purpose
-    """,
+        Flow, automation, constraint checking is based on given 3 projects uniquely.
+            """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Smarten Technology",
+    'license': 'LGPL-3',
+    'sequence':'-10',
+    'website': "https://www.smarten.com.np",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Services',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','industry_fsm'],
 
     # always loaded
     'data': [
